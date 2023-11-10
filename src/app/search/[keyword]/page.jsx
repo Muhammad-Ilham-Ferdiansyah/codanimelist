e3e4f1a990a773f948613f38c58facc49864d612
Muhammad-Ilham-Ferdiansyah <ilhamferdiansyah737@gmail.com>
2023-11-10 11:25:19 +0700

    Rename page file to .jsx and fix errors

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,19 +1,18 @@
-'use client'
-
 import AnimeList from "@/components/AnimeList"
 import Header from "@/components/AnimeList/header";
 
 const Page = async ({params}) => {
-    const { keyword } = params;
+  const { keyword } = params;
+  const decodedKeyword = decodeURI(keyword);
   //pemanggilan API Public
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`);
   const searchAnime = await response.json();
 
     return (
       <>
         {/* Anime Pencarian */}
         <section>
-          <Header title={`Pencarian Untuk ${keyword}...`} />
+          <Header title={`Pencarian Untuk ${decodedKeyword}...`} />
           <AnimeList api={searchAnime}/>
         </section>
 
